afd2c822801d285275600f8b9f9f110801dca8d3
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-06-17 02:43:19 -0500

    able to grab url of clicked iamge

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,9 +10,6 @@ document.getElementById("submit-btn").addEventListener("click", function(){
 search = document.getElementById("input-text").value;
 var url = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ZqL2rZ2Z072B1Hh0MMCdToU2zWH7OqVT" + "&limit=" + limit;
 
-
-
-
 $.ajax({
   url: url,
   method: 'GET',
@@ -21,14 +18,18 @@ $.ajax({
   $("#display").empty();
     var image = result.data;
     for (var i = 0; i < image.length; i++) {
-        var img = document.createElement("img");
-        img.className = "render";
-        img.src = image[i].images.original_still.url;
-        img.attributes = "still" + i;
-        document.getElementById("display").appendChild(img);
-        img.addEventListener("click", function(){
-            console.log(img.attributes);
-        })
+            var img = document.createElement("img");
+            img.className = "render";
+            img.src = image[i].images.original_still.url;
+            img.attributes = "still" + i;
+            document.getElementById("display").appendChild(img);
+                if (typeof window.addEventListener === "function") {
+                img.addEventListener("click", function(){
+                console.log(this)
+                console.log(img.src)
+                
+                })
+        }
     }
 
 }).fail(function(err) {
