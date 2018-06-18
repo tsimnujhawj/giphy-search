a97385c91df2b8253706688c9fc896b910cc89ad
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-06-18 16:29:41 -0500

    fix

---
+++ giphy/logic.js
@@ -0,0 +1,67 @@
+$(document).ready(function() { // DOCUMENT READY OPENING
+
+var tags = ["dog", "dolphin", "whale", "cat", "elephant", "otter"];
+var limit = 10;
+
+// Function for displaying movie data
+  function renderButtons() {
+    $("#tags").empty();
+    for (var i = 0; i < tags.length; i++) {
+      $("#tags").append("<button class='tag-buttons btn btn-primary'>" + tags[i] + "</button>");
+    }
+  }
+
+// Add tags function // 
+
+$(document).on("click", "#addTag", function(event) {
+
+  event.preventDefault();
+
+  var newTag = $("#category").val().trim();
+  tags.push(newTag);
+
+  $("#tags").append("<button class='tag-buttons btn btn-primary'>" + newTag + "</button>");
+
+});
+
+// Tag button function //
+
+$(document).on("click", ".tag-buttons", function(event) {
+
+  // Keeps page from reloading //
+  event.preventDefault();
+
+  var type = this.innerText;
+  console.log(this.innerText);
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + window.encodeURI(type) + "&limit=" + limit + "&api_key=ZqL2rZ2Z072B1Hh0MMCdToU2zWH7OqVT";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    for (var i = 0; i < response.data.length; i++) {
+
+      $("#photo").append('<img class="gif" src="' + response.data[i].images.original_still.url + '">');
+    }  
+  });
+
+  $("#photo").empty();
+
+});
+renderButtons();
+
+
+$("body").on("click", ".gif", function() {
+    var src = $(this).attr("src");
+  if($(this).hasClass('playing')){
+     //stop
+     $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
+     $(this).removeClass('playing');
+  } else {
+    //play
+    $(this).addClass('playing');
+    $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+  }
+});
+
+}); // DOCUMENT READY CLOSING
