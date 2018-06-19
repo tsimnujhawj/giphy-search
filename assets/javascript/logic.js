507cf4188709b1e4c6b24b7ca150b9f4d78ca6ae
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-06-18 20:46:07 -0500

    fix

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,54 +1,60 @@
 $(document).ready(function() { // DOCUMENT READY OPENING
 
-var search;
-var limit = 5;
-
-// Submit Button
-document.getElementById("submit-btn").addEventListener("click", function(){
-search = document.getElementById("input-text").value;
-var url = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ZqL2rZ2Z072B1Hh0MMCdToU2zWH7OqVT" + "&limit=" + limit;
-
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-
-  $("#display").empty();
-    var image = result.data;
-    for (var i = 0; i < image.length; i++) {
-            var img = document.createElement("img");
-            img.className = "render";
-            var animate_image = image[i].images.original.url;
-            var still_image = image[i].images.original_still.url;
-            img.src = still_image;
-            document.getElementById("display").appendChild(img);
-                if (typeof window.addEventListener === "function") {
-                img.addEventListener("click", function(){
-                this.src = animate_image;
-                })
-        }
+  var tags = ["javascript", "rick and morty", "code", "css", "shia motivation", "ducks", "tigers"];
+  var limit = 10;
+  
+  // Display category buttons
+    function render() {
+      $("#tags").empty();
+      for (var i = 0; i < tags.length; i++) {
+        $("#tags").append("<button class='tag-buttons btn'>" + tags[i] + "</button>");
+      }
     }
-
-}).fail(function(err) {
-  throw err;
-});
-})
-
-
-// var funcs = [];
-
-// function createfunc(i) {
-//     return function() {
-//       console.log("My value: " + i);
-//     };
-// }
-
-// for (var i = 0; i < 3; i++) {
-//     funcs[i] = createfunc(i);
-// }
-
-// for (var j = 0; j < 3; j++) {
-//     funcs[j]();
-// }
-
-}); // DOCUMENT READY CLOSING
+  
+  // Submit function to add category
+  $(document).on("click", "#addTag", function(event) {
+    event.preventDefault();
+    var newTag = $("#category").val().trim();
+    tags.push(newTag);
+    $("#tags").append("<button class='tag-buttons btn'>" + newTag + "</button>");
+  });
+  
+  // Tag button function
+  $(document).on("click", ".tag-buttons", function(event) {
+    event.preventDefault();
+    var type = this.innerText;
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + window.encodeURI(type) + "&limit=" + limit + "&api_key=ZqL2rZ2Z072B1Hh0MMCdToU2zWH7OqVT";
+  
+  // AJAX call for JSON data
+    $.ajax({
+      url: url,
+      method: "GET"
+    }).done(function(response) {
+      for (var i = 0; i < response.data.length; i++) {
+  
+  // Append images to DOM
+        $("#photo").append('<img class="gif" src="' + response.data[i].images.fixed_height_still.url + '">');
+      }  
+    });
+    $("#photo").empty();
+  
+  });
+  
+  // Render the initial buttons
+  render();
+  
+  
+  $("body").on("click", ".gif", function() {
+      var src = $(this).attr("src");
+    if($(this).hasClass("animate")){
+       // Pause gif
+       $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
+       $(this).removeClass("animate");
+    } else {
+      // Activate animate
+      $(this).addClass("animate");
+      $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+    }
+  });
+  
+  }); // DOCUMENT READY CLOSING
